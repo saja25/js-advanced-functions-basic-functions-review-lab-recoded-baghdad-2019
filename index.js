38f7c07a77e447be818890af4ddf6c87090bb37f
saja amer <sajaamer86@gmail.com>
2019-09-30 11:27:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(input="roller-skate"){
+  return `This Saturday, I want to ${input}!`
+}
+function mondayWork(input="go to the office"){
+  return `This Monday, I will ${input}.`
+}
+function wrapAdjective(wrap="*"){
+  return function (input="special"){
+    return `You are ${wrap}${input}${wrap}!`
+  }
+}
